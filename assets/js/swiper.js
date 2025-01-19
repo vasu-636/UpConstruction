764b23e97ae60cef73b272a21c169f7ee53d3ea1
Vasu Patel <vasupatel632006@gmail.com>
2025-01-19 21:20:50 +0530

    Add files via upload
    all file uploaded

---
+++ assets/js/swiper.js
@@ -0,0 +1,9 @@
+// const swiper = new Swiper('.swiper', {
+//     slidesPerView: 2, // Display 2 slides at a time
+//     spaceBetween: 20, // Space between slides in pixels
+//     loop: true, // Enable infinite loop
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+//   });
